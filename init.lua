97a125aeee6445645e7392f8aea89c32954de4b6
thi <thi@gurkenbruehe.de>
2023-07-16 21:48:18 +0200

    Run goimport and gofmt on save

--- init.lua
+++ init.lua
@@ -523,6 +523,16 @@ cmp.setup {
   },
 }
 
+-- go.vim: run goimport and gofmt on save
+local format_sync_grp = vim.api.nvim_create_augroup("GoImport", {})
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*.go",
+  callback = function()
+   require('go.format').goimport()
+  end,
+  group = format_sync_grp,
+})
+
 vim.cmd.colorscheme('solarized')
 
 -- Key mappings
